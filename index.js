f2e896cde7b50c70eeafe203c12edbd42de3509c
David Birgersson <david.birgersson@quickspin.com>
2018-01-30 13:25:08 +0100

    Fixes copy paste errors

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-n getTimeRemaining(endtime) {
+function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
@@ -38,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date().setFullYear(2018, 4, 30)));
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
